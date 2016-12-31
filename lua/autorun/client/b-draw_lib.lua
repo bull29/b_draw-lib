75a1d1a6cbd890ef2d755b1d3e4d36f09f6690cb
Bull <thebull2140@gmail.com>
2016-12-31 12:29:31 -0500

    rotated webimages

--- lua/autorun/client/b-draw_lib.lua
+++ lua/autorun/client/b-draw_lib.lua
@@ -4,22 +4,28 @@
     You can use this anywhere for any purpose as long as you acredit the work to the original author with this notice.
     Optionally, if you choose to use this within your own software, it would be much appreciated if you could inform me of it.
     I love to see what people have done with my code! :)
-]]
---
+]]--
+
 file.CreateDir("downloaded_assets")
+
 local exists = file.Exists
 local write = file.Write
 local fetch = http.Fetch
-local white = Color(255, 255, 255)
+local white = Color( 255, 255, 255 )
 local surface = surface
 local crc = util.CRC
 local _error = Material("error")
+
 local mats = {}
 local fetchedavatars = {}
 
 local function fetch_asset(url)
 	if not url then return _error end
-	if mats[url] then return mats[url] end
+
+	if mats[url] then
+		return mats[url]
+	end
+
 	local crc = crc(url)
 
 	if exists("downloaded_assets/" .. crc .. ".png", "DATA") then
@@ -38,31 +44,36 @@ local function fetch_asset(url)
 	return mats[url]
 end
 
-local function fetchAvatarAsset(id64, size)
+local function fetchAvatarAsset( id64, size )
 	size = size == "medium" and "medium" or size == "small" and "" or size == "large" and "full" or ""
-	if fetchedavatars[id64 .. " " .. size] then return fetchedavatars[id64 .. " " .. size] end
-	fetchedavatars[id64 .. " " .. size] = "http://i.imgur.com/uaYpdq7.png"
 
-	fetch("http://steamcommunity.com/profiles/" .. id64 .. "/?xml=1", function(body)
+	if fetchedavatars[ id64 .. " " .. size ] then
+		return fetchedavatars[ id64 .. " " .. size ]
+	end
+
+	fetchedavatars[ id64 .. " " .. size ] = "http://i.imgur.com/uaYpdq7.png"
+
+	fetch("http://steamcommunity.com/profiles/" .. id64 .. "/?xml=1",function( body )
 		local link = body:match("http://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/.-jpg")
 		if not link then return end
-		fetchedavatars[id64 .. " " .. size] = link:Replace(".jpg", (size ~= "" and "_" .. size or "") .. ".jpg")
+
+		fetchedavatars[ id64 .. " " .. size ] = link:Replace( ".jpg", ( size ~= "" and "_" .. size or "") .. ".jpg")
 	end)
 end
 
-function draw.WebImage(url, x, y, width, height, color, angle)
+function draw.WebImage( url, x, y, width, height, color, angle )
 	color = color or white
-	surface.SetDrawColor(color.r, color.g, color.b, color.a)
-	surface.SetMaterial(fetch_asset(url))
 
+	surface.SetDrawColor( color.r, color.g, color.b, color.a )
+	surface.SetMaterial( fetch_asset( url ) )
 	if not angle then
-		surface.DrawTexturedRect(x, y, width, height)
+		surface.DrawTexturedRect( x, y, width, height)
 	else
-		surface.DrawTexturedRectRotated(x, y, width, height, angle)
+		surface.DrawTexturedRectRotated( x, y, width, height, angle )
 	end
 end
 
-function draw.SteamAvatar(avatar, res, x, y, width, height, color)
-	local url = fetchAvatarAsset(avatar, res)
-	draw.WebImage(url, x, y, width, height, color)
+function draw.SteamAvatar( avatar, res, x, y, width, height, color )
+	local url = fetchAvatarAsset( avatar, res )
+	draw.WebImage( url, x, y, width, height, color )
 end
